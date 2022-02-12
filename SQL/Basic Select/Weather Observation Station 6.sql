4c97e93ba65a5cbd83cea3369efaa5e34f1bbf0b
Hibro <1feyfk@gmail.com>
2022-02-12 15:50:27 +0300

    Все города с гласной буквы, без дубликатов

--- SQL/Basic Select/Weather Observation Station 6.sql
+++ SQL/Basic Select/Weather Observation Station 6.sql
@@ -7,3 +7,10 @@ WHERE CITY REGEXP '^[aeiou]';
 --  [...]     |  Any character listed between the square brackets (Любой символ указанный в квадратных скобках)
 
 -- Useful Reference (Полезная ссылка) : https://www.tutorialspoint.com/mysql/mysql-regexps.htm
+
+
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. 
+-- Your result cannot contain duplicates.
+
+-- Запросите список названий ГОРОДА, начинающихся с гласных (например, a, e, i, o или u), из STATION. 
+-- Ваш результат не может содержать дубликатов.
