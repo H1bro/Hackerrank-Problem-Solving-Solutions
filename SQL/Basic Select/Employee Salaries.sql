c606f37e4e189f8ff97e2f2287e6b178249a8998
Hibro <1feyfk@gmail.com>
2022-02-12 18:07:45 +0300

    Список сотрудников по критерию

---
+++ SQL/Basic Select/Employee Salaries.sql
@@ -0,0 +1,17 @@
+SELECT name
+FROM Employee
+WHERE salary > 2000 AND months < 10
+ORDER BY employee_id
+
+
+---------------------------------------
+
+-- Write a query that prints a list of employee names 
+-- (i.e.: the name attribute) for employees in Employee 
+-- having a salary greater than $2000 per month who have been 
+-- employees for less than 10 months. Sort your result by ascending employee_id.
+
+-- Напишите запрос, который печатает список имен сотрудников 
+-- (т. е. атрибут name) для сотрудников в Employee 
+-- с зарплатой более 2000 долларов в месяц, которые 
+-- проработали менее 10 месяцев. Отсортируйте результат по возрастанию employee_id.
