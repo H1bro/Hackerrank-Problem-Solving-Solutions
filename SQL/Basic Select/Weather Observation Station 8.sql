7bc7405a6a065201234be9edb914894c05f16f0a
Hibro <1feyfk@gmail.com>
2022-02-12 16:04:46 +0300

    Все города с гласной в начале и в конце

--- SQL/Basic Select/Weather Observation Station 8.sql
+++ SQL/Basic Select/Weather Observation Station 8.sql
@@ -9,3 +9,10 @@ WHERE CITY REGEXP '^[aeiou](.*)[aeiou]$';
 --  [...]     |  Any character listed between the square brackets (Любой символ указанный в квадратных скобках)
 
 -- Useful Reference (Полезная ссылка) : https://www.tutorialspoint.com/mysql/mysql-regexps.htm
+
+
+-- Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) 
+-- as both their first and last characters. Your result cannot contain duplicates.
+
+-- Запросите список названий ГОРОДА из STATION, в которых гласные (т. е. a, e, i, o и u) 
+-- стоят как в первом, так и в последнем символе. Ваш результат не может содержать дубликатов.
