6399197a66dee982839a80626efd1b8d59f59d3b
Hibro <1feyfk@gmail.com>
2022-02-12 15:20:18 +0300

    Update Japanese Cities' Attributes.sql

--- SQL/Basic Select/Japanese Cities' Attributes.sql
+++ SQL/Basic Select/Japanese Cities' Attributes.sql
@@ -1,2 +1,5 @@
 SELECT * FROM CITY
 WHERE COUNTRYCODE = 'JPN';
+
+-- Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+-- Запросите все атрибуты каждого японского города в таблице CITY. КОД СТРАНЫ для Японии — JPN.
