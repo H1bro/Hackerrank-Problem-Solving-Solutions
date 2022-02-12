343da1b6f72d08db114eedf14a532972f1719882
Hibro <1feyfk@gmail.com>
2022-02-12 14:34:46 +0300

    Create Weather Observation Station 9.sql

---
+++ SQL/Basic Select/Weather Observation Station 9.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY REGEXP '^[^aeiou]';
+
+--  Pattern (Шаблон)   | What the pattern matches (Что соответствует шаблону)
+---------------------------------------
+--    ^       |  Beginning of string (Начало строки)
+--    $       |  End of string (Конец строки)
+--   (.*)    |  Include any symbols in the match (Включать все символы в совпадение)
+--  [...]     |  Any character listed between the square brackets (Любой символ указанный в квадратных скобках)
+
+-- Useful Reference (Полезная ссылка) : https://www.tutorialspoint.com/mysql/mysql-regexps.htm
