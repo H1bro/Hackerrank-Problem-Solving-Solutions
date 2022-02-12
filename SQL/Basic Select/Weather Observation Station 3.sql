b3bf96d0f6b584a732335685582ec89d7ab4f3df
Hibro <1feyfk@gmail.com>
2022-02-12 15:26:57 +0300

    Update Weather Observation Station 3.sql

--- SQL/Basic Select/Weather Observation Station 3.sql
+++ SQL/Basic Select/Weather Observation Station 3.sql
@@ -8,3 +8,7 @@ WHERE ID % 2 = 0;
 SELECT DISTINCT CITY
 FROM STATION
 WHERE MOD(ID, 2) = 0
+
+
+-- Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer. 
+-- Запросите список названий ГОРОДА из STATION для городов с четным идентификационным номером. Выведите результаты в любом порядке, но исключите дубликаты из ответа.
