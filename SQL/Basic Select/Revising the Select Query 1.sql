0c57becca6e0b5621286d445f1f1919f290f8734
Hibro <1feyfk@gmail.com>
2022-02-12 15:13:57 +0300

    Update Revising the Select Query 1.sql

--- SQL/Basic Select/Revising the Select Query 1.sql
+++ SQL/Basic Select/Revising the Select Query 1.sql
@@ -1,1 +1,5 @@
 SELECT * FROM CITY WHERE CountryCode = 'USA' AND Population > 100000;
+
+
+-- Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+-- Запросите все столбцы для всех американских городов в таблице CITY с населением более 100 000 человек. CountryCode для Америки — USA.
