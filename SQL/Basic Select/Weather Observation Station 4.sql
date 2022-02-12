a7621b90c8e86e0c14f267dc163e2a344e1e0276
Hibro <1feyfk@gmail.com>
2022-02-12 15:29:33 +0300

    Разница между общим количеством записей и количеством отдельных записей

--- SQL/Basic Select/Weather Observation Station 4.sql
+++ SQL/Basic Select/Weather Observation Station 4.sql
@@ -1,2 +1,6 @@
 SELECT COUNT(CITY) - COUNT(DISTINCT CITY)
 FROM STATION;
+
+
+-- Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+-- Найдите разницу между общим количеством записей CITY в таблице и количеством отдельных записей CITY в таблице.
