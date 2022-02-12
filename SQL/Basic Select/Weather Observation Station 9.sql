afb11454d5834ccbc397a69746fc85bac738b20d
Hibro <1feyfk@gmail.com>
2022-02-12 16:08:25 +0300

    Города которые не начинаются с гласной

--- SQL/Basic Select/Weather Observation Station 9.sql
+++ SQL/Basic Select/Weather Observation Station 9.sql
@@ -9,3 +9,10 @@ WHERE CITY REGEXP '^[^aeiou]';
 --  [...]     |  Any character listed between the square brackets (Любой символ указанный в квадратных скобках)
 
 -- Useful Reference (Полезная ссылка) : https://www.tutorialspoint.com/mysql/mysql-regexps.htm
+
+
+-- Query the list of CITY names from STATION that do not start with vowels. 
+-- Your result cannot contain duplicates.
+
+-- Запросите список названий ГОРОДА из STATION, которые не начинаются с гласных. 
+-- Ваш результат не может содержать дубликатов.
