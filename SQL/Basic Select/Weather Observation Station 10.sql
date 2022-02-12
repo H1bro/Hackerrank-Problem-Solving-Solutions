67e41433fd1120fbff7d5ba2c4e00b9354b107eb
Hibro <1feyfk@gmail.com>
2022-02-12 16:10:37 +0300

    Города которые не заканчиваются на гласную

--- SQL/Basic Select/Weather Observation Station 10.sql
+++ SQL/Basic Select/Weather Observation Station 10.sql
@@ -9,3 +9,10 @@ WHERE CITY REGEXP '[^aeiou]$';
 --  [...]     |  Any character listed between the square brackets (Любой символ указанный в квадратных скобках)
 
 -- Useful Reference (Полезная ссылка) : https://www.tutorialspoint.com/mysql/mysql-regexps.htm
+
+
+-- Query the list of CITY names from STATION that do not end with vowels. 
+-- Your result cannot contain duplicates.
+
+-- Запросите список названий ГОРОДА из STATION, которые не заканчиваются на гласные. 
+-- Ваш результат не может содержать дубликатов.
