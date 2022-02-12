450769b8588b33c49e21fb2ac2baa7e51d844792
Hibro <1feyfk@gmail.com>
2022-02-12 15:22:00 +0300

    Update Japanese Cities' Names.sql

--- SQL/Basic Select/Japanese Cities' Names.sql
+++ SQL/Basic Select/Japanese Cities' Names.sql
@@ -1,2 +1,5 @@
 SELECT NAME FROM CITY
 WHERE COUNTRYCODE = 'JPN';
+
+-- Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
+-- Запросите названия всех японских городов в таблице CITY. КОД СТРАНЫ для Японии — JPN.
