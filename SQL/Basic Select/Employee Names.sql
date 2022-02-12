258c60800f07b4dccc8abfb80a45856d18883f08
Hibro <1feyfk@gmail.com>
2022-02-12 17:30:06 +0300

    Имена в алфавитном порядке

---
+++ SQL/Basic Select/Employee Names.sql
@@ -0,0 +1,20 @@
+SELECT NAME
+FROM Employee
+ORDER BY NAME ASC
+
+---------------------------------------
+--   ORDER BY |  Keyword is used to sort the result-set in ascending or descending order.
+              |  Ключевое слово используется для сортировки набора результатов в порядке возрастания или убывания.
+              
+ -- Useful Reference (Полезная ссылка)
+
+https://www.w3schools.com/sql/sql_orderby.asp
+
+---------------------------------------
+
+
+-- Write a query that prints a list of employee names 
+-- (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+-- Напишите запрос, который выводит список имен сотрудников 
+-- (т.е. атрибут name) из таблицы Employee в алфавитном порядке.
