dd63a84e62ce346e474580fe1651e8c124125923
Hibro <1feyfk@gmail.com>
2022-02-12 16:20:31 +0300

    Города не начинаются с гл и не заканч на гл

--- SQL/Basic Select/Weather Observation Station 12.sql
+++ SQL/Basic Select/Weather Observation Station 12.sql
@@ -12,5 +12,9 @@ WHERE CITY REGEXP '^[^aeiou](.*)[^aeiou]$';
 
 -- Useful Reference (Полезная ссылка) : https://www.tutorialspoint.com/mysql/mysql-regexps.htm
 
--- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
--- Запросите список названий ГОРОДА из STATION, которые не начинаются с гласных и не заканчиваются на гласные. Ваш результат не может содержать дубликатов.
+
+-- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. 
+-- Your result cannot contain duplicates.
+
+-- Запросите список названий ГОРОДА из STATION, которые не начинаются с гласных и не заканчиваются на гласные. 
+-- Ваш результат не может содержать дубликатов.
