ecee908727980acc16eac1e9836bdce9ae48278c
Hibro <1feyfk@gmail.com>
2022-02-18 15:50:35 +0300

    Евклидово расстояние между точками

--- SQL/Aggregation/Weather Observation Station 19.sql
+++ SQL/Aggregation/Weather Observation Station 19.sql
@@ -23,7 +23,7 @@ https://www.w3schools.com/sql/func_sqlserver_power.asp
 ---------------------------------------
 
 -- Consider P1(a,b) and P2(c,d) to be two points on a 2D plane where  are the respective minimum and maximum values of Northern Latitude (LAT_N) and  are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
-Query the Euclidean Distance between points  and  and format your answer to display  decimal digits.
+Query the Euclidean Distance between points P1 and P2 and format your answer to display 4 decimal digits.
 
 -- Рассмотрим P1(a,b) и P2(c,d) как две точки на двумерной плоскости, где соответствующие минимальное и максимальное значения северной широты (LAT_N) и соответствующие минимальное и максимальное значения западной долготы (LONG_W) в STATION.
-Запросите евклидово расстояние между точками и отформатируйте ответ для отображения десятичных цифр.
+Запросите евклидово расстояние между точками P1 и P2 и отформатируйте ответ для отображения 4 десятичных цифр.
