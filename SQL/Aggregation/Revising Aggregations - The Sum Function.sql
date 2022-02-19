fca5cf71bbcbc3134f825146184ddc8222e60f17
Hibro <1feyfk@gmail.com>
2022-02-19 19:36:43 +0300

    Общая численность по критерию

---
+++ SQL/Aggregation/Revising Aggregations - The Sum Function.sql
@@ -0,0 +1,22 @@
+SELECT SUM(POPULATION) FROM CITY
+WHERE DISTRICT = 'California';
+
+---------------------------------------
+
+--   SUM()    |  The SUM() function returns the total sum of a numeric column. 
+              |  Функция SUM() возвращает общую сумму числового столбца.
+              
+
+
+-- Useful Reference (Полезная ссылка)
+
+https://www.w3schools.com/sql/sql_count_avg_sum.asp
+
+
+
+---------------------------------------
+
+-- Query the total population of all cities in CITY where District is California.
+
+
+-- Запросите общую численность населения всех городов в CITY, где District — Калифорния.
