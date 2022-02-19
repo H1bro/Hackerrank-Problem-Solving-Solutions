aa0738d34224d6669cba18e0ded174ecc439c8e8
Hibro <1feyfk@gmail.com>
2022-02-19 20:22:33 +0300

    Разница между макс и мин значением

---
+++ SQL/Aggregation/Population Density Difference.sql
@@ -0,0 +1,21 @@
+SELECT MAX(POPULATION) - MIN(POPULATION) FROM CITY
+
+---------------------------------------
+
+--   MIN()    |  The MIN() function returns the smallest value of the selected column.
+              |  Функция MIN() возвращает наименьшее значение выбранного столбца.
+              
+--   MAX().   |  The MAX() function returns the largest value of the selected column.
+              |  Функция MAX() возвращает наибольшее значение выбранного столбца.
+              
+
+-- Useful Reference (Полезная ссылка)
+
+https://www.w3schools.com/sql/sql_min_max.asp
+
+
+---------------------------------------
+
+-- Query the difference between the maximum and minimum populations in CITY.
+
+-- Запросите разницу между максимальной и минимальной численностью населения в CITY.
