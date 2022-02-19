b739c3d1ea8dc2e94626929820e6c0570abae413
Hibro <1feyfk@gmail.com>
2022-02-19 19:40:09 +0300

    Средняя численность по критерию

---
+++ SQL/Aggregation/Revising Aggregations - Averages.sql
@@ -0,0 +1,22 @@
+SELECT AVG(POPULATION) FROM CITY
+WHERE DISTRICT = 'California';
+
+
+---------------------------------------
+
+--   AVG()    |  The AVG() function returns the average value of a numeric column. 
+              |  Функция AVG() возвращает среднее значение числового столбца.
+              
+
+
+-- Useful Reference (Полезная ссылка)
+
+https://www.w3schools.com/sql/sql_count_avg_sum.asp
+
+
+
+---------------------------------------
+
+-- Query the average population of all cities in CITY where District is California.
+
+-- Запросить среднюю численность населения всех городов в CITY, где District — Калифорния.
