802e3ae93810dc0617ada52850027b1ace399f48
Hibro <1feyfk@gmail.com>
2022-02-19 19:26:14 +0300

    Количество строк соответствующих условию

---
+++ SQL/Aggregation/Revising Aggregations - The Count Function.sql
@@ -0,0 +1,22 @@
+SELECT COUNT(*) FROM CITY WHERE population > 100000
+
+
+---------------------------------------
+
+--   COUNT()  |  The COUNT() function returns the number of rows that matches a specified criterion.
+              |  Функция COUNT() возвращает количество строк, соответствующих заданному критерию.
+              
+
+
+-- Useful Reference (Полезная ссылка)
+
+https://www.w3schools.com/sql/sql_count_avg_sum.asp
+
+
+
+---------------------------------------
+
+-- Query a count of the number of cities in CITY having a Population larger than 100 000.
+
+
+-- Запрос количества городов в CITY с населением более 100 000 человек.
