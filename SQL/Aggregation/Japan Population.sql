fe996cd61ef0ebf67a6760dbda46e95f1088da8f
Hibro <1feyfk@gmail.com>
2022-02-19 20:11:58 +0300

    Сумма столбца по критерию

---
+++ SQL/Aggregation/Japan Population.sql
@@ -0,0 +1,20 @@
+SELECT SUM(POPULATION) FROM CITY
+WHERE COUNTRYCODE = 'JPN'
+
+---------------------------------------
+
+--   SUM()    |  The SUM() function returns the total sum of a numeric column. 
+              |  Функция SUM() возвращает общую сумму числового столбца.
+              
+              
+
+-- Useful Reference (Полезная ссылка)
+
+https://www.w3schools.com/sql/sql_count_avg_sum.asp
+
+
+---------------------------------------
+
+-- Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+
+-- Запросите сумму населения для всех японских городов в CITY. КОД СТРАНЫ для Японии — JPN.
