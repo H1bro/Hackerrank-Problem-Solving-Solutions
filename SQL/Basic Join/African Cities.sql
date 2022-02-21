cf16c62f2edaff99375ab0ff82b4b56a25b31360
Hibro <1feyfk@gmail.com>
2022-02-21 22:10:18 +0300

    Названия городов, по условию

---
+++ SQL/Basic Join/African Cities.sql
@@ -0,0 +1,13 @@
+SELECT CITY.NAME
+FROM CITY, COUNTRY
+WHERE CITY.COUNTRYCODE = COUNTRY.CODE AND CONTINENT = 'AFRICA'
+
+---------------------------------------
+
+
+
+---------------------------------------
+
+-- query the names of all cities where the CONTINENT is 'Africa'.
+
+-- запросите названия всех городов, где КОНТИНЕНТ — «Африка».
